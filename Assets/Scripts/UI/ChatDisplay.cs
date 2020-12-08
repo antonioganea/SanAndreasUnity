994d11f59b8d3bd11afec849deee17c0b6dbe895
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2020-12-09 00:08:59 +0200

    Better chat (#97)
    * Initial chat commit
    
    * Disable input when chat is opened
    
    * Maximum messages limit
    
    * Fading
    
    * Final touches

--- Assets/Scripts/UI/ChatDisplay.cs
+++ Assets/Scripts/UI/ChatDisplay.cs
@@ -1,65 +1,75 @@
-﻿using System.Collections.Generic;
+﻿using SanAndreasUnity.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using SanAndreasUnity.Utilities;
-using UnityEngine.UI;
-using System.Linq;
+
 
 namespace SanAndreasUnity.UI
 {
-
     public class ChatDisplay : MonoBehaviour
     {
+        public static ChatDisplay Instance { get; private set; }
+
+        public TMP_InputField inputField;
+        public TMP_Text text;
+
+        CustomInput customInput;
+
+        static UnityEngine.EventSystems.EventSystem eventSystemComponent;
 
         Queue<Chat.ChatMessage> m_chatMessages = new Queue<Chat.ChatMessage>();
-
         public int maxNumChatMessages = 5;
-        public float timeToRemoveMessage = 3f;
 
+        bool currentlyFading = false;
 
+        public float timeToHideChat = 5f;
+
+        void Awake()
+        {
+            Instance = this;
+        }
 
         void Start()
         {
+            eventSystemComponent = UnityEngine.EventSystems.EventSystem.current;
+
             Chat.ChatManager.onChatMessage += OnChatMsg;
+            inputField.onSubmit.AddListener((s) => SendChatMessage(s));
+
+            customInput = CustomInput.Instance;
         }
 
-        void OnChatMsg(Chat.ChatMessage chatMsg)
-		{
-			if (m_chatMessages.Count >= this.maxNumChatMessages)
-				m_chatMessages.Dequeue();
-			
-			m_chatMessages.Enqueue(chatMsg);
+        static bool lastFrameIsOpened = false;
 
-            if (!this.IsInvoking(nameof(RemoveMessage)))
-                this.Invoke(nameof(RemoveMessage), this.timeToRemoveMessage);
-
-            this.UpdateUI();
-		}
-
-        void RemoveMessage()
+        public static bool IsOpened
         {
-            if (m_chatMessages.Count > 0)
-                m_chatMessages.Dequeue();
-            
-            // invoke again if there are more messages
-            if (m_chatMessages.Count > 0)
-                this.Invoke(nameof(RemoveMessage), this.timeToRemoveMessage);
-
-            this.UpdateUI();
-        }
-
-        void UpdateUI()
-        {
-            Text[] texts = this.gameObject.GetFirstLevelChildrenComponents<Text>().ToArray();
-            Chat.ChatMessage[] chatMessages = m_chatMessages.ToArray();
-
-            for (int i = 0; i < texts.Length; i++)
-            {
-                if (i < chatMessages.Length)
-                    texts[i].text = GetDisplayTextForChatMessage(chatMessages[i]);
+            get {
+                if (Instance != null)
+                {
+                    bool condition = eventSystemComponent.currentSelectedGameObject == Instance.inputField.gameObject;
+                    bool output = condition || lastFrameIsOpened;
+                    lastFrameIsOpened = condition;
+                    return output;
+                }
                 else
-                    texts[i].text = "";
+                {
+                    return false;
+                }
             }
+        }
 
+        void Update()
+        {
+            if ( customInput != null)
+            {
+                if (customInput.GetKeyDown(KeyCode.T))
+                {
+                    inputField.gameObject.SetActive(true);
+                    inputField.Select();
+                    UnFade();
+                }
+            }
         }
 
         string GetDisplayTextForChatMessage(Chat.ChatMessage chatMessage)
@@ -67,6 +77,91 @@ namespace SanAndreasUnity.UI
             return "<color=blue>" + chatMessage.sender + "</color> : " + chatMessage.msg;
         }
 
-    }
+        void OnChatMsg(Chat.ChatMessage chatMsg)
+        {
+            AddMessage(chatMsg);
+            inputField.text = "";
+        }
 
+        void AddMessage(Chat.ChatMessage chatMsg)
+        {
+            if (m_chatMessages.Count >= this.maxNumChatMessages)
+                m_chatMessages.Dequeue();
+
+            m_chatMessages.Enqueue(chatMsg);
+
+            this.UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            Chat.ChatMessage[] chatMessages = m_chatMessages.ToArray();
+
+            string textStr = "";
+
+            for ( int i = 0; i < chatMessages.Length; i++)
+            {
+                textStr += GetDisplayTextForChatMessage(chatMessages[i]) + "\n";
+            }
+
+            text.text = textStr;
+
+            UnFade();
+            SetFadeTimeout();
+        }
+
+        IEnumerator FadeChat()
+        {
+            // fade from opaque to transparent
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                if (!currentlyFading)
+                    yield break;
+
+                // set color with i as alpha
+                text.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+
+        void StartFade()
+        {
+            if ( currentlyFading == false)
+            {
+                currentlyFading = true;
+                StartCoroutine(FadeChat());
+            }
+        }
+
+        void SetFadeTimeout()
+        {
+            if (!this.IsInvoking(nameof(StartFade)))
+                this.Invoke(nameof(StartFade), this.timeToHideChat);
+        }
+
+        void UnFade()
+        {
+            currentlyFading = false;
+            text.color = new Color(1, 1, 1, 1);
+        }
+
+        void SendChatMessage(string msg)
+        {
+            eventSystemComponent.SetSelectedGameObject(null);
+            SetFadeTimeout();
+
+            inputField.text = "";
+
+            inputField.gameObject.SetActive(false);
+
+            if (!this.IsInvoking(nameof(StartFade)))
+                this.Invoke(nameof(StartFade), 5f);
+
+            if (string.IsNullOrWhiteSpace(msg))
+                return;
+
+            Chat.ChatManager.SendChatMessageToAllPlayersAsLocalPlayer(msg);
+        }
+    }
 }
